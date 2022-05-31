1fde62a2d1241395aeb5be7679c259b8882db6ae
Agathe Lurquin <agathe.lurquin@essca.eu>
2022-05-31 14:27:15 -0300

    dinings routes and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
-  # has_many :dinings (role: host)
-  has_many :dinings, through: :bookings # role: guest
+  has_many :dinings
 
   validates :email, presence: true, uniqueness: true
   validates :address, :country, :name, :role, :password, presence: true
