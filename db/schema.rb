9b4800b247a330a4c1bd51ab7cb363a0b1b2df3d
54rtor1 <1929sartori@gmail.com>
2022-05-31 15:55:20 -0300

    comment column added to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_145600) do
+ActiveRecord::Schema.define(version: 2022_05_31_185319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_05_31_145600) do
     t.string "status", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "comment"
     t.index ["dining_id"], name: "index_bookings_on_dining_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
