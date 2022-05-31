a5825a1fd91bf50a8c34b4ac63475ced93b5d0ad
Agathe Lurquin <agathe.lurquin@essca.eu>
2022-05-31 12:23:18 -0300

    booking model and model validations

--- app/models/dining.rb
+++ app/models/dining.rb
@@ -1,3 +1,6 @@
 class Dining < ApplicationRecord
-  belongs_to :user
+  belongs_to :user # host
+  has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings # guest
+  validates :date, :time, :category, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bookings
+  # has_many :dinings (role: host)
+  has_many :dinings, through: :bookings # role: guest
+
+  validates :email, presence: true, uniqueness: true
+  validates :address, :country, :name, :role, :password, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
