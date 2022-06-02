46a249cc0477cbde76771ab90ebf3bb3d87156f0
Adriana Osorio <driosorio@gmail.com>
2022-06-02 13:18:45 -0300

    picture upload done

--- app/models/dining.rb
+++ app/models/dining.rb
@@ -5,4 +5,5 @@ class Dining < ApplicationRecord
   validates :date, :time, :category, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_one_attached :photo
 end
