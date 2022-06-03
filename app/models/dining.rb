3aa844c7de8ae799cf3a79d1377f1b7dd048bc0f
Adriana Osorio <driosorio@gmail.com>
2022-06-03 12:25:25 -0300

    fixing user model validations

--- app/models/dining.rb
+++ app/models/dining.rb
@@ -2,7 +2,7 @@ class Dining < ApplicationRecord
   belongs_to :user # host
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings # guest
-  validates :date, :time, :category, presence: true
+  validates :date, :time, :category, :name, :address, :max_guests, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_one_attached :photo
@@ -11,6 +11,6 @@ class Dining < ApplicationRecord
   pg_search_scope :search_by_address,
     against: [ :address ],
     using: {
-      tsearch: { prefix: true } 
+      tsearch: { prefix: true }
     }
 end
